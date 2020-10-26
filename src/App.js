605a20f6de23f14e9049fc72008e2e958a9c4fd5
MayTzur <49557739+MayTzur@users.noreply.github.com>
2020-10-26 21:19:55 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import WebLayout from './Components/Web';
+
+//<React.StrictMode></React.StrictMode>
+
+function App() {
+  return (<WebLayout/>);
+}
+
+export default App;
